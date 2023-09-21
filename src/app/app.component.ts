257fb8c0caa4f2df098beda791a8cfb7d5e4abda
hamza112255 <ameeralihamza750@gmail.com>
2023-09-21 15:08:51 +0500

    contact number

---
+++ src/app/app.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { COUNTRIES } from './countries';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  countries = COUNTRIES;
+  code: string = '+92';
+  myForm: FormGroup;
+  countryName: string | undefined;
+  number: string = '';
+  countryCode: string = '';
+  shortCode: string = '';
+  flag: string = '';
+  isValidPhoneNumber: boolean = false;
+  isEmpty: boolean = false;
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      phoneNo: ['', [Validators.required, this.validatePhoneNumber.bind(this)]],
+      countryName: ['', Validators.required],
+      countryCode: ['', [Validators.required]],
+    });
+    this.shortCode = 'Country';
+    console.log(this.shortCode);
+  }
+  validatePhoneNumber(control: any) {
+    const phoneNumber = control.value;
+    if (phoneNumber.length === 0) {
+      this.isEmpty = true;
+    } else {
+      this.isEmpty = false;
+    }
+    const phoneRegex = /^\d{7,17}$/;
+    if (phoneRegex.test(phoneNumber)) {
+      return null;
+    } else {
+      return { invalidPhoneNumber: true };
+    }
+  }
+  onSelectionChange(event: any) {
+    this.countryName = event.value;
+    for (const country of this.countries) {
+      if (this.countryName === country.name) {
+        this.myForm.get('countryCode')?.setValue(country.phone);
+        this.shortCode = country.iso['alpha-2'];
+        this.flag = country.image;
+      }
+    }
+  }
+  onSubmit() {
+    if (this.myForm.valid) {
+      this.number = this.myForm.get('phoneNo')?.value;
+      this.countryCode = this.myForm.get('countryCode')?.value;
+    }
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatIconModule } from '@angular/material/icon';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ConcatenatePipe } from './concatenate.pipe';
+@NgModule({
+  declarations: [AppComponent, ConcatenatePipe],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
